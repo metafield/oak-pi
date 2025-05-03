4e088ddfd7e238bb157bdad464d8cf8592a77f4a
metafield <anthony.sylvester99@gmail.com>
2025-05-02 17:21:17 -0700

    test: led test

--- oak.ts
+++ oak.ts
@@ -1,10 +1,24 @@
 import { Application, Router } from '@oak/oak';
+import { Gpio } from 'npm:onoff';
 
 export function startServer() {
   const router = new Router();
 
   router.get('/', (ctx) => {
     ctx.response.body = 'Hello world';
+  });
+
+  router.post('/toggle_led', async (ctx) => {
+    console.log(await ctx.request.body.text());
+
+    const led = new Gpio(17, 'out');
+
+    Deno.addSignalListener('SIGINT', () => {
+      console.log('SIGINT received (Ctrl+C)');
+      led.unexport(); // or your equivalent cleanup
+      console.log('interrupted!');
+      Deno.exit();
+    });
   });
 
   const app = new Application();
